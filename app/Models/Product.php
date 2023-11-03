336e4985a3892397546404b86349b8ffb27b1302
morihisashi <hisa.1995.322@outlook.com>
2023-11-03 14:03:49 +0900

    productと機能の追加

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Product extends Model
 {
     use HasFactory;
-    public function products()
+    public function category()
      {
-        return $this->hasMany('App\Models\Product');
+        return $this->belongsTo('App\Models\Category');
      }
 }
