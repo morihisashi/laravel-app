ede165981a5cdc657019e53650ae2cc5f626a0f7
morihisashi <hisa.1995.322@outlook.com>
2023-11-06 22:39:43 +0900

    レビュー機能の追加

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,4 +12,9 @@ class Product extends Model
      {
         return $this->belongsTo('App\Models\Category');
      }
+
+     public function reviews()
+     {
+         return $this->hasMany('App\Models\Review');
+     }
 }
